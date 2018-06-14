cbe606f55a562f9d8d2459c3794f009f711e1fc0
tanghongying <1483515764@qq.com>
2018-06-15 01:10:29 +0800

    添加plugins插件部分

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     devtool:'cheap-source-map',
     entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/dist", //打包后的文件存放的地方  二笔的错误少了一个斜杠路径问题
+        path: __dirname + "/build", //打包后的文件存放的地方  二笔的错误少了一个斜杠路径问题
         filename: "budle.js" //打包后输出文件的文件名
     },
     devServer: {
         contentBase: "./dist", //本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
-        inline: true//实时刷新
+        inline: true, //实时刷新
+        hot: true
     },
 
     // 配置Babel
@@ -37,10 +40,20 @@ module.exports = {
                             modules: true, // 指定启用CSS modules
                             localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
                         }
+                    },
+                    {
+                        loader: "postcss-loader"
                     }
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html" ////new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin() //热加载组件
+    ],
 };
 // 注：“__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
